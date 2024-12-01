6aa3a52e678bd1106a0fbe2830c4b7421d88d18d
samco <it026@cht-group.net>
2024-12-01 16:21:44 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+.button{
+  position: relative;
+}
+.upload-input {
+  position: absolute;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
