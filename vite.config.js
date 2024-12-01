bc047d80cc2ea5abc43448a1671896114906348a
samco <it026@cht-group.net>
2024-12-01 17:58:55 +0800

    更新打包路径，部署文件

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src") // 路径别名
     }
   },
+  base: "./",
+  build: { outDir: "docs" },
   server: {
     proxy: {
       "/api": {
